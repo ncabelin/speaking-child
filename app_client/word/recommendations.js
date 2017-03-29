66de7ad42b13c89b478941958fc61d84eec5619d
Michael Cabelin <ncabelin@gmail.com>
2017-03-28 23:55:20 -0700

    add recommendations for words

---
+++ app_client/word/recommendations.js
@@ -0,0 +1,18 @@
+(function() {
+	var recommendation = {
+		food: ['apple'],
+		toys: [''],
+		outdoors: [''],
+		animals: [''],
+		body: [''],
+		places: [''],
+		actions: [''],
+		household: [''],
+		personal: [''],
+		people: [''],
+		clothes: [''],
+		vehicles: [''],
+		modifiers: [''],
+		others: ['']
+	}
+})();
